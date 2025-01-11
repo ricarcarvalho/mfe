0a015a3ad04bbeb7bd5bed8821ece3765858efc4
ricarcarvalho <ricarcarvalho@deloitte.pt>
2025-01-11 14:41:21 +0000

    restructured project

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
 
-import Landing from "./components/Landing";
-import Pricing from "./components/Pricing";
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
 
 export default () => {
   return (
     <div>
       <StylesProvider>
         <BrowserRouter>
-          <Switch></Switch>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
         </BrowserRouter>
       </StylesProvider>
     </div>
